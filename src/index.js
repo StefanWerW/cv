e479f0c4bf10155e1e80852820299ee4bc5698f0
StefanWerW <scaw.dev@gmail.com>
2018-07-25 23:35:53 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import registerServiceWorker from './registerServiceWorker';
+import Resume from 'react-awesome-resume'
+import 'semantic-ui-css/semantic.min.css';
+
+const myJSONResume = require("./resume.json")
+
+ReactDOM.render(
+  <Resume jsonResume={myJSONResume} theme="default" />,
+  document.getElementById("root")
+);
+registerServiceWorker();
